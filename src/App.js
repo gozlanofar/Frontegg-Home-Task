3f3860764495923bc37e0311e3f03bc710e17094
gozlanofar <nofarg1997@gmail.com>
2025-07-10 16:29:16 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useAuth, useLoginWithRedirect, ContextHolder, AdminPortal } from "@frontegg/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { user, isAuthenticated } = useAuth();
+const loginWithRedirect = useLoginWithRedirect();
+
+useEffect(() => {
+if (!isAuthenticated) {
+loginWithRedirect();
 }
+}, [isAuthenticated, loginWithRedirect]);
+
+const logout = () => {
+const baseUrl = ContextHolder.for().getContext().baseUrl;
+window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location.href}`;
+};
+const handleClick = () => {
+  AdminPortal.show();
+};
+
+return (
+
+<div className="App">
+  {isAuthenticated ? (
+    <div>
+      <div>
+        <img src={user?.profilePictureUrl} alt={user?.name} />
+      </div>
+      <div>
+        <span>Logged in as: {user?.name}</span>
+      </div>
+      <div>
+        <button onClick={() => logout()}>Click to logout</button>
+      </div>
+      <div>
+        <button onClick={handleClick}>settings</button>
+      </div>
+    </div>
+  ) : (
+    <div>
+      <button onClick={() => loginWithRedirect()}>Click me to login</button>
+    </div>
+  )}
+</div>
+); }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.js'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+baseUrl: 'https://app-57np1g03k1ku.frontegg.com',
+clientId: 'd43caac5-f915-431d-9fd2-c99fd32c71bf',
+appId: '82cbc0c7-d957-464a-b7ad-6dd6d2477d4c'
+};
+
+const authOptions = {
+keepSessionAlive: true // Uncomment this in order to maintain the session alive
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<FronteggProvider
+  contextOptions={contextOptions}
+  hostedLoginBox={true}
+  authOptions={authOptions}
+>
+  <App />
+</FronteggProvider>
+, document.getElementById('root') ); 

89f3d73c37db334a840239f811e98cd49667574e
gozlanofar <nofarg1997@gmail.com>
2025-07-17 11:50:09 +0300

    add drop down tenant switch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import { useEffect } from 'react';
-import { useAuth, useLoginWithRedirect, ContextHolder, AdminPortal } from "@frontegg/react";
+import { useEffect,useState } from 'react';
+import { useAuth, useLoginWithRedirect, ContextHolder, AdminPortal,useTenantsActions } from "@frontegg/react";
 
 function App() {
-const { user, isAuthenticated } = useAuth();
+const { user, isAuthenticated,tenantsState } = useAuth();
 const loginWithRedirect = useLoginWithRedirect();
 
 useEffect(() => {
@@ -19,6 +19,19 @@ window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${windo
 const handleClick = () => {
   AdminPortal.show();
 };
+const { loadTenants,switchTenant } = useTenantsActions();
+const [selectedTenant, setSelectedTenant] = useState(''); 
+useEffect(() => {
+  if (isAuthenticated){
+    loadTenants();
+  }
+}, []);
+const handleChange = (e) => {
+    const tenantId = e.target.value;
+    if (!tenantId || tenantId === user?.tenantId) return;
+    setSelectedTenant(tenantId);
+    switchTenant({ tenantId });
+  };
 
 return (
 
@@ -37,6 +50,18 @@ return (
       <div>
         <button onClick={handleClick}>settings</button>
       </div>
+      {tenantsState?.tenants?.length > 0 && (
+        <div>
+          <select value={selectedTenant} onChange={handleChange}>
+            <option value="" disabled>Change tenant</option>
+            {tenantsState.tenants.map((tenant) => (
+              <option key={tenant.id} value={tenant.id}>
+                {tenant.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
     </div>
   ) : (
     <div>
